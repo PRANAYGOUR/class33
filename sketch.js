3f345196423d354022a75387f0263f36bbb28415
PRANAYGOUR <pranaygour3042007@gmail.com>
2021-02-23 19:40:55 +0530

    debugs

--- sketch.js
+++ sketch.js
@@ -95,6 +95,12 @@ function mouseReleased(){
 function keyPressed(){
     if(keyCode === 32){
        slingshot.attach(bird.body);
+       bird.trajectory = [];
+       Matter.Body.setPosition(bird.body, {x:200, y:200});
+       
+
+
+
     }
 }
 
